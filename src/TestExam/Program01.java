f9982fba037b60f50e0e85bf3b43e024aaff6fca
Soumen Banerjee <soumen8653@gmail.com>
2021-03-18 01:26:54 +0530

    c2

--- src/TestExam/Program01.java
+++ src/TestExam/Program01.java
@@ -5,14 +5,14 @@ public class Program01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
+
 		System.out.println("Hi");
 		System.out.println("WB");
-=======
+
 		System.out.println("Hello");
 		System.out.println("India");
 
->>>>>>> branch 'master' of https://github.com/soumenb10/JavaProject.git
+
 	}
 
 }
