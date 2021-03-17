7c625862d1308a7eb1c103d9e1ab7ee822c52a06
Soumen Banerjee <soumen8653@gmail.com>
2021-03-18 01:22:27 +0530

    c1

--- src/TestExam/Program01.java
+++ src/TestExam/Program01.java
@@ -5,8 +5,8 @@ public class Program01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello");
-
+		System.out.println("Hi");
+		System.out.println("WB");
 	}
 
 }
